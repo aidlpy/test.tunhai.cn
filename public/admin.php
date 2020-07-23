d68bcf0e4e25dd8f9431f9d4a7f0277113511dfe
Albert <admin@aidlpy.com>
2020-07-23 16:33:44 +0800

    update

---
+++ public/admin.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zhangxinrong
+ * Date: 2020/7/23
+ * Time: 3:08 PM
+ */
+
+
+// 定义应用目录
+define('APP_PATH', __DIR__ . '/../application/admin');
+// 加载框架引导文件
+require __DIR__ . '/../thinkphp/start.php';
